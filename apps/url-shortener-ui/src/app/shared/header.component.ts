afa14f10eea4af41bde2772dac329b081cc70a03
Souvik Datta <dattasouvik1997@gmail.com>
2025-03-16 21:34:20 +0530

    feat: added theme toggling

--- apps/url-shortener-ui/src/app/shared/header.component.ts
+++ apps/url-shortener-ui/src/app/shared/header.component.ts
@@ -1,15 +1,58 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
+
+
+enum THEME {
+  light = 'light',
+  dark = 'dark'
+}
 
 @Component({
   selector: 'app-header',
   template: `
     <nav class="nav-bar">
-      <div class="container h-20 text-center flex items-center justify-between">
+      <div class="container px-2 h-20 text-center flex items-center justify-between">
         <button class="icon-btn"><i class="material-icons">apps</i></button>
         <a class="text-4xl font-medium" href="/">Minify URL</a>
-        <button class="icon-btn"><i class="material-icons">dark_mode</i></button>
+        <button class="icon-btn" (click)="toggleTheme()" title="Toggle Theme">
+          @if(theme === 'light') {
+            <i class="material-icons">dark_mode</i>
+          }
+          @else {
+            <i class="material-icons">light_mode</i>
+          }
+        </button>
       </div>
     </nav>
   `,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  THEME = THEME; // Expose enum to the template
+  private _theme: THEME;
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this._theme = (localStorage.getItem('theme') as THEME) || THEME.light;
+    this.applyTheme();
+  }
+
+  get theme(): THEME {
+    return this._theme;
+  }
+
+  private set theme(value: THEME) {
+    this._theme = value;
+    localStorage.setItem('theme', value);
+    this.applyTheme();
+  }
+
+  toggleTheme() {
+    this.theme = this.theme === THEME.light ? THEME.dark : THEME.light;
+  }
+
+  private applyTheme() {
+    this.renderer.setAttribute(this.document.body, 'data-theme', this._theme);
+  }
+}
